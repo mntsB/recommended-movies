231543cdcfb2668ddd1e6c59400d60d494bab4ec
Mantas Buga <mantasbuga1997@gmail.com>
2019-05-08 22:41:48 +0300

    Updates

---
+++ precache-manifest.41435d4a2e75288d043a05fb4c6fc611.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5d6139a73b4310c3ae5a6929f7b103",
+    "url": "/recommended-movies/index.html"
+  },
+  {
+    "revision": "3d44a2283a6caa2f40fb",
+    "url": "/recommended-movies/static/css/main.fdce8ac7.chunk.css"
+  },
+  {
+    "revision": "8f4fdc53389c548307b0",
+    "url": "/recommended-movies/static/js/2.3b7a6730.chunk.js"
+  },
+  {
+    "revision": "3d44a2283a6caa2f40fb",
+    "url": "/recommended-movies/static/js/main.6cdc50b6.chunk.js"
+  },
+  {
+    "revision": "daea9020acf413c054fe",
+    "url": "/recommended-movies/static/js/runtime~main.d0e8a4ff.js"
+  },
+  {
+    "revision": "86548632653d3c6ce3a8c3d50887adca",
+    "url": "/recommended-movies/static/media/A-bugs-life.86548632.png"
+  },
+  {
+    "revision": "2851d4f6ef188ce83015bf1d92b1d04a",
+    "url": "/recommended-movies/static/media/All-quiet-on-the-western-front.2851d4f6.png"
+  },
+  {
+    "revision": "dd6344b6efed4d090b50127e4e2fd970",
+    "url": "/recommended-movies/static/media/Aquaman.dd6344b6.png"
+  },
+  {
+    "revision": "13b6064e51f8cd837af678cdf3c85e16",
+    "url": "/recommended-movies/static/media/Avenger-endgame.13b6064e.png"
+  },
+  {
+    "revision": "f136450a825ec59747f89b8423f6577c",
+    "url": "/recommended-movies/static/media/Black-panther.f136450a.png"
+  },
+  {
+    "revision": "5392ff868c3c3389320944e39c72793e",
+    "url": "/recommended-movies/static/media/Bumblebee.5392ff86.png"
+  },
+  {
+    "revision": "3118e073b21e5169b8194514f3e12fe4",
+    "url": "/recommended-movies/static/media/Captain-marvel.3118e073.png"
+  },
+  {
+    "revision": "50844f8d0c6eb858b6174bca622c2f08",
+    "url": "/recommended-movies/static/media/Glass.50844f8d.png"
+  },
+  {
+    "revision": "28a6c4a040f1d0eb5eccbf8f6933c8c6",
+    "url": "/recommended-movies/static/media/Green-book.28a6c4a0.png"
+  },
+  {
+    "revision": "6f3c467dcf4447fa16f15f9f490aa2a4",
+    "url": "/recommended-movies/static/media/Scrollbar-arrow-right.6f3c467d.svg"
+  },
+  {
+    "revision": "53f27dd99cd40ebf8b9b2c46ff402d85",
+    "url": "/recommended-movies/static/media/Scrollbar-arrow.53f27dd9.svg"
+  },
+  {
+    "revision": "5becbe4a8ec0bc44d6b38aabf6c52b19",
+    "url": "/recommended-movies/static/media/The-highwaymen.5becbe4a.png"
+  }
+]);
